6215d519df1bf99f613de1e2aafc7136f957bab0
jruizndev <jorsqn@gmail.com>
2025-01-29 23:18:50 +0100

    feat: add date input for interactive countdown selection - Added input element in index.html for user selected countdown date - Styled input field in styles.css for better appearance - Updated main.js to dynamically update countdown based on user selection

--- main.js
+++ main.js
@@ -1,36 +1,33 @@
+// Obtener la referencia al input de fecha
+const dateSelect = document.getElementById('dateSelect')
+
 // Función para actualizar el contador
 const actualizador = setInterval(function () {
     // Definimos el tiempo de inicio en milisegundos
-    const myBirthday = new Date('May 30, 2025 00:00:00').getTime()
-    console.log(myBirthday)
+    const targetDate = dateSelect.value
+        ? new Date(dateSelect.value).getTime()
+        : new Date().getTime()
 
     // Definimos el tiempo actual
     const now = new Date().getTime()
-    console.log(now)
 
     // Calculamos la diferencia entre el tiempo de inicio y el tiempo actual
-    let timeRemaining = myBirthday - now
-    console.log(timeRemaining)
+    let timeRemaining = targetDate - now
 
     // Convertimos los milisegundos a segundos 1s = 1000ms
     timeRemaining /= 1000
-    console.log(timeRemaining)
 
     // Calculamos los días restantes
     const days = Math.floor(timeRemaining / (60 * 60 * 24))
-    console.log(days)
 
     // Calculamos las horas restantes
     const hours = Math.floor((timeRemaining % (60 * 60 * 24)) / (60 * 60))
-    console.log(hours)
 
     // Calculamos los minutos restantes
     const minutes = Math.floor((timeRemaining % (60 * 60)) / 60)
-    console.log(minutes)
 
     // Calculamos los segundos restantes
     const seconds = Math.floor(timeRemaining % 60)
-    console.log(seconds)
 
     // Obtenemos el elemento HTML donde se mostrará el contador
     const timer = document.getElementById('timer')
