e08573f38c7578d13ee4379240865001614138d0
jruizndev <jorsqn@gmail.com>
2025-01-29 21:36:12 +0100

    refactor: update countdown to refresh dynamically using setInterval

--- main.js
+++ main.js
@@ -1,40 +1,42 @@
-// Definimos el tiempo de inicio en milisegundos
-const myBirthday = new Date('May 30, 2025 00:00:00').getTime()
-console.log(myBirthday)
+// Función para actualizar el contador
+const actualizador = setInterval(function () {
+    // Definimos el tiempo de inicio en milisegundos
+    const myBirthday = new Date('May 30, 2025 00:00:00').getTime()
+    console.log(myBirthday)
 
-// Definimos el tiempo actual
-const now = new Date().getTime()
-console.log(now)
+    // Definimos el tiempo actual
+    const now = new Date().getTime()
+    console.log(now)
 
-// Calculamos la diferencia entre el tiempo de inicio y el tiempo actual
-let timeRemaining = myBirthday - now
-console.log(timeRemaining)
+    // Calculamos la diferencia entre el tiempo de inicio y el tiempo actual
+    let timeRemaining = myBirthday - now
+    console.log(timeRemaining)
 
-// Convertimos los milisegundos a segundos 1s = 1000ms
-timeRemaining /= 1000
-console.log(timeRemaining)
+    // Convertimos los milisegundos a segundos 1s = 1000ms
+    timeRemaining /= 1000
+    console.log(timeRemaining)
 
-// Calculamos los días restantes
-const days = Math.floor(timeRemaining / (60 * 60 * 24))
-console.log(days)
+    // Calculamos los días restantes
+    const days = Math.floor(timeRemaining / (60 * 60 * 24))
+    console.log(days)
 
-// Calculamos las horas restantes
-const hours = Math.floor((timeRemaining % (60 * 60 * 24)) / (60 * 60))
-console.log(hours)
+    // Calculamos las horas restantes
+    const hours = Math.floor((timeRemaining % (60 * 60 * 24)) / (60 * 60))
+    console.log(hours)
 
-// Calculamos los minutos restantes
-const minutes = Math.floor((timeRemaining % (60 * 60)) / 60)
-console.log(minutes)
+    // Calculamos los minutos restantes
+    const minutes = Math.floor((timeRemaining % (60 * 60)) / 60)
+    console.log(minutes)
 
-// Calculamos los segundos restantes
-const seconds = Math.floor(timeRemaining % 60)
-console.log(seconds)
+    // Calculamos los segundos restantes
+    const seconds = Math.floor(timeRemaining % 60)
+    console.log(seconds)
 
-// Obtenemos el elemento HTML donde se mostrará el contador
-const timer = document.getElementById('timer')
+    // Obtenemos el elemento HTML donde se mostrará el contador
+    const timer = document.getElementById('timer')
 
-// Agregamos los elementos HTML al contenedor
-timer.innerHTML = `
+    // Agregamos los elementos HTML al contenedor
+    timer.innerHTML = `
 <div>
   <span>${days}</span>
   <span class="text">Días</span>
@@ -52,3 +54,4 @@ timer.innerHTML = `
   <span class="text">Segundos</span>
   </div>
   `
+}, 1000)
